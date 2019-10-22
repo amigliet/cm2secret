5455c7bb783d9425172e2c1b41de6e93418598bb
Andrea Miglietta <amigliet92@gmail.com>
2019-10-23 00:47:35 +0200

    main.go: add functions and update main()

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
   "encoding/json"
+  "flag"
   "fmt"
+  "gopkg.in/yaml.v2"
   "io/ioutil"
+  "log"
   "os"
 )
 
@@ -27,43 +30,95 @@ type Secret struct {
 }
 
 func NewConfigMap() ConfigMap {
-  return ConfigMap{"", map[string]string{}, "ConfigMap", map[string]string{}}
+  return ConfigMap{"v1", map[string]string{}, "ConfigMap", map[string]string{}}
 }
 
 func NewSecret() Secret {
   return Secret{"v1", map[string][]byte{}, "Secret", map[string]string{}}
 }
 
+func (c *ConfigMap) loadConfigMap(b []byte) error {
+  if json.Valid(b) {
+    return json.Unmarshal(b, c)
+  }
+  return yaml.Unmarshal(b, c)
+}
+
+func (c *ConfigMap) CM2Secret() Secret {
+  // Create a new Secret object
+  s := NewSecret()
+
+  s.ApiVersion = c.ApiVersion
+  s.Metadata["name"] = c.Metadata["name"]
+
+  for key, value := range c.Data {
+    s.Data[key] = []byte(value)
+  }
+  return s
+}
+
+func JSONToYAML(j []byte) ([]byte, error) {
+	var jsonObj interface{}
+
+	err := yaml.Unmarshal(j, &jsonObj)
+	if err != nil {
+		return nil, err
+	}
+	return yaml.Marshal(jsonObj)
+}
+
+func fileExists(file string) bool {
+  _, err := os.Stat(file)
+  return err == nil
+}
 
 func main(){
 
-  // FIX: blocking line if no stdin is provided
-  bytes, err := ioutil.ReadAll(os.Stdin)
+  var (
+    inputFile    = flag.String("f", "", "ConfigMap filename to convert.")
+    outputFormat = flag.String("o", "json", "Output format. Choose 'json' or 'yaml'.")
+  )
+  flag.Parse()
+
+  // TODO: Set up initial checks.
+  if !fileExists(*inputFile) {
+    log.Fatalf("ERROR: Input file does not exist.")
+  }
+
+  bytes, err := ioutil.ReadFile(*inputFile)
   if err != nil {
-    panic(err)
+    log.Fatalf("ERROR: Cannot read input file: %s", err)
   }
 
-  var parsed map[string]interface{}
-  if err := json.Unmarshal(bytes, &parsed); err != nil {
-    panic(err)
+  var cm ConfigMap
+  if err = cm.loadConfigMap(bytes); err != nil {
+    log.Fatalf("ERROR: Invalid input format: %s", err)
   }
 
-  // Create a new Secret object
-  sec := NewSecret()
+  // Create a new Secret object from loaded ConfigMap
+  sec := cm.CM2Secret()
 
-  for key, value := range parsed["data"].(map[string]interface{}) {
-    sec.Data[key] = []byte(value.(string))
-  }
-
-  sec.Metadata["name"] = parsed["metadata"].(map[string]interface{})["name"].(string)
-
+  // NOTE: Even if the desired output is in YAML, this is a mandatory step.
+  //       Because when a Secret object is directly marshaled into YAML,
+  //       map[string][]byte data are not serialized and encoded properly.
   var jsonData []byte
   jsonData, err = json.Marshal(sec)
   if err != nil {
-    fmt.Println(err)
+    log.Fatalf("ERROR: Failed to marshal secret: %s", err)
   }
 
-  // Print the results to standard output
-  fmt.Println(string(jsonData))
+  switch *outputFormat {
+    case "json":
+      fmt.Println(string(jsonData))
+    case "yaml":
+      yamlData, err := JSONToYAML(jsonData)
+      if err != nil {
+        log.Fatalf("ERROR: cannot marshaling YAML: %s", err)
+      }
+      fmt.Println(string(yamlData))
+    default:
+      fmt.Println(string(jsonData))
+  }
 
 }
+
